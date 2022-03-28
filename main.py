08a08e0a7616859b7be82a0605937bc66b1844a5
ShawnReardon <shawnreardon1@gmail.com>
2022-03-28 01:06:13 +0000

    ai vs ai

--- main.py
+++ main.py
@@ -6,25 +6,38 @@ from time import sleep
 # creating enumerations using class
 
 com = AI()
+com2 = AI()
 moves = {"rock":1, "paper":2, "scissors":3}
 p1_wins = 0
 p2_wins = 0
 resultsDic = {("rock", "scissors")}
 #winCon = ((Moves(0),Moves(0), Results(3)))
 exit = ''
-def play():
+def play(isPlayer):
+  playerVsCom = isPlayer
   global exit
   debug = False
   exit = ''
-  while exit not in range(0,4):
-    exit = randint(1,3) #int(input("Type \n 1 for rock \n 2 for paper \n 3 for scissors \n > "))
-  assert exit in range(0,4), "Not in range 1-3"
-  p = exit
-  c = Moves(com.checkTendencies()).name
-  com.storeMove(Moves(p).name)
-  if debug: 
-    print(com.oppMoves)
-  return(Moves(p).name,c)
+  if playerVsCom:
+    while exit not in range(0,4):
+      exit = randint(1,3) #int(input("Type \n 1 for rock \n 2 for paper \n 3 for scissors \n > "))
+    assert exit in range(0,4), "Not in range 1-3"
+    p = exit
+    c = Moves(com.checkTendencies()).name
+ 
+    com.storeMove(Moves(p).name)
+    if debug: 
+      print(com.oppMoves)
+    return(Moves(p).name,c)
+  else:
+    c = Moves(com.checkTendencies()).name
+    c2 = Moves(com2.checkTendencies()).name
+    com.storeMove(c2)
+    com2.storeMove(c)
+    if debug: 
+      print(com.oppMoves)
+    return(c2,c)
+  
 
 def determineResults(results):
   if result[0] == 'rock' and result[1] == 'paper':
@@ -42,7 +55,7 @@ def determineResults(results):
   else:
     return Results(3).name
 while exit != 0:
-  result = play()
+  result = play(False)
   print()
   print('P1: ', result[0])
   print('P2: ', result[1])
